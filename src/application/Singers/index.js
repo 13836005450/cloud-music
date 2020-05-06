e2ed4ae99b2f732ec184fd7c3d52a3e8d7375f8c
cheny <597985642@qq.com>
2020-05-06 19:20:44 +0800

    歌手页的导航栏 横向列表

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,10 +1,35 @@
 //src/appliction/Singers/index.js
-import React from 'react';
+import React, {useState} from 'react';
+import Horizen from '../../baseUI/horizen-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import { NavContainer } from "./style";
 
-function Home (props) {
+function Singers () {
+  let [category, setCategory] = useState ('');
+  let [alpha, setAlpha] = useState ('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha (val);
+  }
+
+  let handleUpdateCatetory = (val) => {
+    setCategory (val);
+  }
+
   return (
-    <div>Singers</div>
+    <NavContainer>
+      <Horizen 
+        list={categoryTypes} 
+        title={"分类 (默认热门):"} 
+        handleClick={handleUpdateCatetory} 
+        oldVal={category}></Horizen>
+      <Horizen 
+        list={alphaTypes} 
+        title={"首字母:"} 
+        handleClick={val => handleUpdateAlpha (val)} 
+        oldVal={alpha}></Horizen>
+    </NavContainer>
   )
 }
 
-export default React.memo (Home);
+export default React.memo (Singers);
